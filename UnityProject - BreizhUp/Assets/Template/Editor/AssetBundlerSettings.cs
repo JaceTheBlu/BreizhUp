54c58595d1418295d17f02efea1485dbb14a0a98
Lucas Baran <lucas.baran7@gmail.com>
2023-11-12 21:30:04 +0100

    add AssetBundlerSettings with provider

---
+++ UnityProject - BreizhUp/Assets/Template/Editor/AssetBundlerSettings.cs
@@ -0,0 +1,65 @@
+﻿using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+[FilePath(ASSET_PATH, FilePathAttribute.Location.ProjectFolder)]
+public class AssetBundlerSettings : ScriptableSingleton<AssetBundlerSettings>
+{
+    /// <summary>
+    /// Where the settings asset is saved.
+    /// </summary>
+    private const string ASSET_PATH = "ProjectSettings/AssetBundlerSettings.asset";
+
+    [SerializeField] private string _bundleFileName = "mod.assets";
+    [SerializeField] private string _outputFolder = "content";
+
+    public void Save()
+    {
+        Save(saveAsText: true);
+    }
+
+    public void BuildAssetBundle()
+    {
+        AssetBundler asset_bundler = new AssetBundler(_bundleFileName, _outputFolder);
+        asset_bundler.BuildAssetBundle();
+    }
+
+    public void RemoveAllPrefabMaterials()
+    {
+        if (!AskRemoveAllPrefabMaterials())
+        {
+            return;
+        }
+
+        AssetBundler asset_bundler = new AssetBundler(_bundleFileName, _outputFolder);
+        asset_bundler.RemoveAllPrefabMaterials();
+    }
+
+    public void SetAllPrefabMaterialsToDefault()
+    {
+        if (!AskSetAllPrefabMaterialsToDefault())
+        {
+            return;
+        }
+
+        AssetBundler asset_bundler = new AssetBundler(_bundleFileName, _outputFolder);
+        asset_bundler.SetAllPrefabMaterialsToDefault();
+    }
+
+    private bool AskRemoveAllPrefabMaterials()
+    {
+        return InternalEditorUtility.inBatchMode
+            || EditorUtility.DisplayDialog("Confirm", "Stripping materials from prefabs is an irreversible process. Perform at your own risk.", "Proceed", "Cancel");
+    }
+
+    private bool AskSetAllPrefabMaterialsToDefault()
+    {
+        return InternalEditorUtility.inBatchMode
+            || EditorUtility.DisplayDialog("Confirm", "Changing the materials of prefabs is an irreversible process. Perform at your own risk.", "Proceed", "Cancel");
+    }
+
+    private void OnEnable()
+    {
+        hideFlags &= ~HideFlags.NotEditable;
+    }
+}
---
+++ UnityProject - BreizhUp/Assets/Template/Editor/AssetBundlerSettingsProvider.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundlerSettingsProvider
+{
+    public const string SETTINGS_PATH = "PlateUp!/Asset bundler";
+
+    private static bool _preparationExpanded = false;
+
+    private static void Draw(
+        string search_context
+        )
+    {
+        SerializedObject serialized_object = new SerializedObject(AssetBundlerSettings.instance);
+        SerializedProperty bundle_file_name_property = serialized_object.FindProperty("_bundleFileName");
+        SerializedProperty output_folder_property = serialized_object.FindProperty("_outputFolder");
+
+        EditorGUILayout.PropertyField(bundle_file_name_property);
+        EditorGUILayout.PropertyField(output_folder_property);
+
+        if (GUILayout.Button("Build asset bundle"))
+        {
+            AssetBundlerSettings.instance.BuildAssetBundle();
+        }
+
+        _preparationExpanded = EditorGUILayout.Foldout(_preparationExpanded, "Preparation", toggleOnLabelClick: true);
+
+        if (_preparationExpanded)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Remove all prefab materials"))
+            {
+                AssetBundlerSettings.instance.RemoveAllPrefabMaterials();
+            }
+
+            if (GUILayout.Button("Set all prefab materials to default"))
+            {
+                AssetBundlerSettings.instance.SetAllPrefabMaterialsToDefault();
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+
+        serialized_object.ApplyModifiedPropertiesWithoutUndo();
+    }
+
+    [SettingsProvider]
+    public static SettingsProvider CreateSettingsProvider()
+    {
+        var settings_provider = new SettingsProvider(SETTINGS_PATH, SettingsScope.Project)
+        {
+            label = "Asset bundler",
+            guiHandler = Draw,
+            keywords = new HashSet<string>(new[] { "Asset", "Bundler", "PlateUp" })
+        };
+
+        return settings_provider;
+    }
+}
